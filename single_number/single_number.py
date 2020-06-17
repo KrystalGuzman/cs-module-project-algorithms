de76e791537a1215d1edf3261b7d16821abacdbd
Krystal Guzman <kryguzman@gmail.com>
2020-06-17 13:05:58 -0700

    finishes single_number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,11 +2,16 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+# def single_number(arr):
+#     # sort the array
+#     arr.sort()
+
+#     # look for a skip in pairs
+#     for i in range(0, len(arr), 2):
+#         if arr[i] is not arr[i + 1]:
+#             return arr[i]
 def single_number(arr):
-    # Your code here
-
-    pass
-
+    return 2 * sum(set(arr)) - sum(arr)
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
