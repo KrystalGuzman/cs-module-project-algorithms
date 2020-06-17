045fc584a560b8ca085d344b4e6408d8d873c780
Krystal Guzman <kryguzman@gmail.com>
2020-06-17 15:38:57 -0700

    adds optimized solution

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,19 +2,25 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
-    # finds position in list
-    index = 0
-    for i in range(len(arr)):
-        # moves up in list
-        if arr[i] != 0:
-            arr[index]=arr[i]
-            index+=1
-    #replaces ending with 0's
-    for i in range(index,len(arr)):
-        arr[i]=0
-    return arr
+# def moving_zeroes(arr):
+#     # finds position in list
+#     index = 0
+#     for i in range(len(arr)):
+#         # moves up in list
+#         if arr[i] != 0:
+#             arr[index]=arr[i]
+#             index+=1
+#     #replaces ending with 0's
+#     for i in range(index,len(arr)):
+#         arr[i]=0
+#     return arr
 
+def moving_zeroes(arr):
+    # return just the nonzero values in the array
+    nonzero_integers = [number for number in arr if number != 0]
+
+    # add with zeroes on the right
+    return nonzero_integers + [0] * (len(arr) - len(nonzero_integers))
 
 if __name__ == '__main__':
     # Use the main function here to test out your implementation
