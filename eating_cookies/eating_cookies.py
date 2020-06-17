cd28e684d09f5740117b5f2ea701fbf6462b834c
Krystal Guzman <kryguzman@gmail.com>
2020-06-17 15:03:22 -0700

    adds eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,13 +2,44 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
+def eating_cookies(n, cache=None):
+    # negative cookies left: not possible. Return 0 as a solution.
+    if n < 0:
+        return 0
 
-    pass
+    # exactly 0 cookies left: 1 solution found
+    # exactly 1 cookie left: 1 way to eat
+    elif n == 0 or n == 1:
+        return 1
+
+    # exactly 2 cookies left: 2 ways to eat
+    elif n == 2:
+        return 2
+
+    # define recursive terms
+    # start current term at 3 cookies to eat so all recursive terms are known
+    n_minus_3_cookies = 1
+    n_minus_2_cookies = 1
+    n_minus_1_cookie = 2
+
+    # define a variable to hold result
+    n_cookies = None
+
+    # calculate current term based on previous two terms
+    for i in range(3, n + 1):
+
+        n_cookies = n_minus_3_cookies + n_minus_2_cookies + n_minus_1_cookie
+
+        # update variables by shifting values over
+        n_minus_1_cookie, n_minus_2_cookies, n_minus_3_cookies = n_cookies, n_minus_1_cookie, n_minus_2_cookies
+
+    return n_cookies
+    
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
     num_cookies = 5
+    num_cookies = 50
 
     print(f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to each {num_cookies} cookies")
